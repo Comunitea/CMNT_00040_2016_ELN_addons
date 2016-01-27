d3a10dc871fa55f0f958962fd02f13f29b5077a4
javierjcf <javierjcf@gmail.com>
2016-01-27 19:25:31 +0100

    [FIX] eln_purchase: migrado

--- eln_production/mrp.py
+++ eln_production/mrp.py
@@ -72,127 +72,127 @@ class mrp_bom(osv.osv):
     _constraints = [
         (_check_product, 'BoM line product should not be same as BoM product.', ['product_id']),
     ]
-    def _bom_explode_bis(self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None):
-
-
-
-        """ Finds Products and Work Centers for related BoM for manufacturing order.
-        @param bom: BoM of particular product template.
-        @param product: Select a particular variant of the BoM. If False use BoM without variants.
-        @param factor: Factor represents the quantity, but in UoM of the BoM, taking into account the numbers produced by the BoM
-        @param properties: A List of properties Ids.
-        @param level: Depth level to find BoM lines starts from 10.
-        @param previous_products: List of product previously use by bom explore to avoid recursion
-        @param master_bom: When recursion, used to display the name of the master bom
-        @return: result: List of dictionaries containing product details.
-                 result2: List of dictionaries containing Work Center details.
-        """
-        result, result2 = super(mrp_bom, self)._bom_explode\
-                (self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None)
-
-
-
-        #SI ES PHANTOM SUSTITUYE PRODUCTO POR LISTA DE MATERIALES
-        ##
-                ## Estaba así
-                # def _bom_explode(self, cr, uid, bom, factor, properties=[], addthis=False, level=0, routing_id=False):
-                # """ Finds Products and Work Centers for related BoM for manufacturing order.
-                # @param bom: BoM of particular product.
-                # @param factor: Factor of product UoM.
-                # @param properties: A List of properties Ids.
-                # @param addthis: If BoM found then True else False.
-                # @param level: Depth level to find BoM lines starts from 10.
-                # @return: result: List of dictionaries containing product details.
-                #          result2: List of dictionaries containing Work Center details.
-                # """
-        def _get_vals(wc_use, operators, operators_n, factor, bom, wc, routing):
-
-            qty_per_cycle = self.pool.get('product.uom')._compute_qty(cr, uid, wc_use.uom_id.id, wc_use.qty_per_cycle, bom.product_uom.id)
-            oper = []
-            if operators_n and operators:
-                for op in range(0, (operators_n)):
-                    oper.append(operators[op])
-            return{
-                'name': tools.ustr(wc_use.name) + u' - '  + tools.ustr(bom.product_id.name),
-                'routing_id': routing.id,
-                'workcenter_id': wc.id,
-                'sequence': level+(wc_use.sequence or 0),
-                'operators_ids': oper and [(6,0,oper)] or False,
-                'cycle': wc_use.cycle_nbr * (factor * bom.product_qty),
-                'time_start': wc_use.time_start,
-                'time_stop': wc_use.time_stop,
-                'hour': float((wc_use.operators_number * ((factor * bom.product_qty)/(qty_per_cycle or 1.0)))/(operators_n or 1.0)),
-                'real_time': float((wc_use.operators_number * ((factor * bom.product_qty)/(qty_per_cycle or 1.0)))/(operators_n or 1.0))
-
-                    }
-
-        routing = (routing_id and routing_obj.browse(cr, uid, routing_id)) or bom.routing_id or False
-        if routing:
-            for wc_use in routing.workcenter_lines:
-                wc = wc_use.workcenter_id
-                d, m = divmod(factor, wc_use.workcenter_id.capacity_per_cycle)
-                mult = (d + (m and 1.0 or 0.0))
-                cycle = mult * wc_use.cycle_nbr
-                result2.append({
-                    'name': tools.ustr(wc_use.name) + ' - ' + tools.ustr(bom.product_tmpl_id.name_get()[0][1]),
-                    'workcenter_id': wc.id,
-                    'sequence': level + (wc_use.sequence or 0),
-                    'cycle': cycle,
-                    'hour': float(wc_use.hour_nbr * mult + ((wc.time_start or 0.0) + (wc.time_stop or 0.0) + cycle * (wc.time_cycle or 0.0)) * (wc.time_efficiency or 1.0)),
-                })
-        routing_obj = self.pool.get('mrp.routing')
-        factor = factor / (bom.product_efficiency or 1.0)
-        factor = rounding(factor, bom.product_rounding)
-        if factor < bom.product_rounding:
-            factor = bom.product_rounding
-        result = []
-        result2 = []
-        phantom = False
-        if bom.type == 'phantom' and not bom.bom_line_ids:
-                    # #def _bom_find(self, cr, uid, product_tmpl_id=None, product_id=None, properties=None, context=None):
-                    # """
-                    # Finds BoM for particular product and product uom.
-                    # @param product_tmpl_id: Selected product.
-                    # @param product_uom: Unit of measure of a product.
-                    # @param properties: List of related properties.
-                    # @return: False or BoM id
-                    # newbom = self._bom_find(cr, uid, bom.product_id.id, bom.product_uom.id, properties)
-                    # """
-            newbom = self._bom_find(cr, uid, bom.product_tmpl_id.id, bom.product_id.id, properties)
-
-            if newbom:
-                res = self._bom_explode(cr, uid, self.browse(cr, uid, [newbom])[0], factor*bom.product_qty, properties, level=level+10)
-                result = result + res[0]
-                result2 = result2 + res[1]
-                phantom = True
-            else:
-                phantom = False
-        if not phantom:
-            if newbom and not bom.bom_lines_ids:
-                result.append(
-                {
-                    'name': bom.product_id.name,
-                    'product_id': bom.product_id.id,
-                    'product_qty': bom.product_qty * factor,
-                    'product_uom': bom.product_uom.id,
-                    'product_uos_qty': bom.product_uos and bom.product_uos_qty * factor or False,
-                    'product_uos': bom.product_uos and bom.product_uos.id or False,
-                })
-            routing = (routing_id and routing_obj.browse(cr, uid, routing_id)) or bom.routing_id or False
-            if routing:
-                for wc_use in routing.workcenter_lines:
-                    wc = wc_use.workcenter_id
-                    operators = []
-                    if wc_use.operators_ids:
-                        for oper in wc_use.operators_ids:
-                            operators.append(oper.id)
-                    result2.append(_get_vals(wc_use, operators, wc_use.operators_number, factor, bom, wc, routing))
-
-            for bom2 in bom.bom_lines:
-                res = self._bom_explode(cr, uid, bom2, factor, properties, level=level+10)
-                result = result + res[0]
-                result2 = result2 + res[1]
-        return result, result2
+    # def _bom_explode_bis(self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None):
+    #
+    #
+    #
+    #     """ Finds Products and Work Centers for related BoM for manufacturing order.
+    #     @param bom: BoM of particular product template.
+    #     @param product: Select a particular variant of the BoM. If False use BoM without variants.
+    #     @param factor: Factor represents the quantity, but in UoM of the BoM, taking into account the numbers produced by the BoM
+    #     @param properties: A List of properties Ids.
+    #     @param level: Depth level to find BoM lines starts from 10.
+    #     @param previous_products: List of product previously use by bom explore to avoid recursion
+    #     @param master_bom: When recursion, used to display the name of the master bom
+    #     @return: result: List of dictionaries containing product details.
+    #              result2: List of dictionaries containing Work Center details.
+    #     """
+    #     result, result2 = super(mrp_bom, self)._bom_explode\
+    #             (self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None)
+    #
+    #
+    #
+    #     #SI ES PHANTOM SUSTITUYE PRODUCTO POR LISTA DE MATERIALES
+    #     ##
+    #             ## Estaba así
+    #             # def _bom_explode(self, cr, uid, bom, factor, properties=[], addthis=False, level=0, routing_id=False):
+    #             # """ Finds Products and Work Centers for related BoM for manufacturing order.
+    #             # @param bom: BoM of particular product.
+    #             # @param factor: Factor of product UoM.
+    #             # @param properties: A List of properties Ids.
+    #             # @param addthis: If BoM found then True else False.
+    #             # @param level: Depth level to find BoM lines starts from 10.
+    #             # @return: result: List of dictionaries containing product details.
+    #             #          result2: List of dictionaries containing Work Center details.
+    #             # """
+    #     def _get_vals(wc_use, operators, operators_n, factor, bom, wc, routing):
+    #
+    #         qty_per_cycle = self.pool.get('product.uom')._compute_qty(cr, uid, wc_use.uom_id.id, wc_use.qty_per_cycle, bom.product_uom.id)
+    #         oper = []
+    #         if operators_n and operators:
+    #             for op in range(0, (operators_n)):
+    #                 oper.append(operators[op])
+    #         return{
+    #             'name': tools.ustr(wc_use.name) + u' - '  + tools.ustr(bom.product_id.name),
+    #             'routing_id': routing.id,
+    #             'workcenter_id': wc.id,
+    #             'sequence': level+(wc_use.sequence or 0),
+    #             'operators_ids': oper and [(6,0,oper)] or False,
+    #             'cycle': wc_use.cycle_nbr * (factor * bom.product_qty),
+    #             'time_start': wc_use.time_start,
+    #             'time_stop': wc_use.time_stop,
+    #             'hour': float((wc_use.operators_number * ((factor * bom.product_qty)/(qty_per_cycle or 1.0)))/(operators_n or 1.0)),
+    #             'real_time': float((wc_use.operators_number * ((factor * bom.product_qty)/(qty_per_cycle or 1.0)))/(operators_n or 1.0))
+    #
+    #                 }
+    #
+    #     routing = (routing_id and routing_obj.browse(cr, uid, routing_id)) or bom.routing_id or False
+    #     if routing:
+    #         for wc_use in routing.workcenter_lines:
+    #             wc = wc_use.workcenter_id
+    #             d, m = divmod(factor, wc_use.workcenter_id.capacity_per_cycle)
+    #             mult = (d + (m and 1.0 or 0.0))
+    #             cycle = mult * wc_use.cycle_nbr
+    #             result2.append({
+    #                 'name': tools.ustr(wc_use.name) + ' - ' + tools.ustr(bom.product_tmpl_id.name_get()[0][1]),
+    #                 'workcenter_id': wc.id,
+    #                 'sequence': level + (wc_use.sequence or 0),
+    #                 'cycle': cycle,
+    #                 'hour': float(wc_use.hour_nbr * mult + ((wc.time_start or 0.0) + (wc.time_stop or 0.0) + cycle * (wc.time_cycle or 0.0)) * (wc.time_efficiency or 1.0)),
+    #             })
+    #     routing_obj = self.pool.get('mrp.routing')
+    #     factor = factor / (bom.product_efficiency or 1.0)
+    #     factor = rounding(factor, bom.product_rounding)
+    #     if factor < bom.product_rounding:
+    #         factor = bom.product_rounding
+    #     result = []
+    #     result2 = []
+    #     phantom = False
+    #     if bom.type == 'phantom' and not bom.bom_line_ids:
+    #                 # #def _bom_find(self, cr, uid, product_tmpl_id=None, product_id=None, properties=None, context=None):
+    #                 # """
+    #                 # Finds BoM for particular product and product uom.
+    #                 # @param product_tmpl_id: Selected product.
+    #                 # @param product_uom: Unit of measure of a product.
+    #                 # @param properties: List of related properties.
+    #                 # @return: False or BoM id
+    #                 # newbom = self._bom_find(cr, uid, bom.product_id.id, bom.product_uom.id, properties)
+    #                 # """
+    #         newbom = self._bom_find(cr, uid, bom.product_tmpl_id.id, bom.product_id.id, properties)
+    #
+    #         if newbom:
+    #             res = self._bom_explode(cr, uid, self.browse(cr, uid, [newbom])[0], factor*bom.product_qty, properties, level=level+10)
+    #             result = result + res[0]
+    #             result2 = result2 + res[1]
+    #             phantom = True
+    #         else:
+    #             phantom = False
+    #     if not phantom:
+    #         if newbom and not bom.bom_lines_ids:
+    #             result.append(
+    #             {
+    #                 'name': bom.product_id.name,
+    #                 'product_id': bom.product_id.id,
+    #                 'product_qty': bom.product_qty * factor,
+    #                 'product_uom': bom.product_uom.id,
+    #                 'product_uos_qty': bom.product_uos and bom.product_uos_qty * factor or False,
+    #                 'product_uos': bom.product_uos and bom.product_uos.id or False,
+    #             })
+    #         routing = (routing_id and routing_obj.browse(cr, uid, routing_id)) or bom.routing_id or False
+    #         if routing:
+    #             for wc_use in routing.workcenter_lines:
+    #                 wc = wc_use.workcenter_id
+    #                 operators = []
+    #                 if wc_use.operators_ids:
+    #                     for oper in wc_use.operators_ids:
+    #                         operators.append(oper.id)
+    #                 result2.append(_get_vals(wc_use, operators, wc_use.operators_number, factor, bom, wc, routing))
+    #
+    #         for bom2 in bom.bom_lines:
+    #             res = self._bom_explode(cr, uid, bom2, factor, properties, level=level+10)
+    #             result = result + res[0]
+    #             result2 = result2 + res[1]
+    #     return result, result2
 
     def _bom_explode(self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None):
         """ Finds Products and Work Centers for related BoM for manufacturing order.
