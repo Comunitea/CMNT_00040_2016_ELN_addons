206d03c57f0d8a402f0c00654c6925505ea746f6
javierjcf <javierjcf@gmail.com>
2016-02-10 18:12:34 +0100

    [FIX]eln_reports: Corregido purchase_order.jrxml

--- eln_reports/report/purchase_order/__init__.py
+++ eln_reports/report/purchase_order/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import purchase_order_report_parser
+# import purchase_order_report_parser
