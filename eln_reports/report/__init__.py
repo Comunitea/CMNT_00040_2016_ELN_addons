79179c6c7563b92d20355937b6bc03a4395e3a12
javierjcf <javierjcf@gmail.com>
2016-02-11 13:24:50 +0100

    [FIX]informe Ficha logistica de producto

--- eln_reports/report/__init__.py
+++ eln_reports/report/__init__.py
@@ -18,13 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import barcode_label
 import control_part
-import invoice
 import packing_list
-import planning_report
 import product_datasheet
 import product_logistics_sheet
 import purchase_order
 import sheet_product
-import stock_picking
