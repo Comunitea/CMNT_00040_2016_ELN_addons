b3c7ea0bcc2baa33e1ad12f608b360a2d7f5e61c
javierjcf <javierjcf@gmail.com>
2016-04-27 13:25:59 +0200

    [ADD]eln_custom: Campo registro de compañía pasa de tamaño 64 a 256

---
+++ eln_custom/res_company.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnológicos (<http://www.comunitea.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+
+    _inherit = "res.company"
+
+    company_registry = fields.Char(size=256)
