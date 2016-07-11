1ad46f2301294285da749e181ace142f10f80382
javierjcf <javierjcf@gmail.com>
2016-07-11 14:04:58 +0200

    [ADD]eln_sale_commission: Vista y agrupadores para las lineas de comisión

--- eln_sale_commission/models/settlement.py
+++ eln_sale_commission/models/settlement.py
@@ -18,8 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api
 
 
 class Settlement(models.Model):
@@ -28,4 +27,29 @@ class Settlement(models.Model):
     @api.depends('lines', 'lines.settled_amount')
     def _compute_total(self):
         for record in self:
-            record.total = sum(x.settled_amount for x in record.lines) * (1 - (self.agent.atypical / 100))
+            record.total = sum(x.settled_amount for x in record.lines) * \
+                (1 - (self.agent.atypical / 100))
+
+
+class SettlementLine(models.Model):
+    _inherit = "sale.commission.settlement.line"
+
+    @api.depends('invoice_line')
+    def _get_partner_id(self):
+        for rec in self:
+            partner = False
+            if rec.invoice_line:
+                partner = rec.invoice_line.invoice_id.partner_id
+                if rec.invoice_line.stock_move_id:
+                    sm = rec.invoice_line.stock_move_id
+                    if sm.picking_id and sm.picking_id.partner_id:
+                        partner = sm.picking_id.partner_id
+            if partner:
+                rec.partner_id = partner.commercial_partner_id.id
+
+    partner_id = fields.Many2one('res.partner', string="Partner",
+                                 compute="_get_partner_id", readonly=True,
+                                 store=True)
+    commission = fields.Many2one(
+        comodel_name="sale.commission", related="agent_line.commission",
+        store=True)
