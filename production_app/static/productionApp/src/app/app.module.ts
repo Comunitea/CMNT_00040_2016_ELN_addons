6a48859fc73b992eee49c4e1129f2926fad83932
javierjcf <javierjcf@gmail.com>
2018-02-19 12:03:35 +0100

    [FIX]production_app: Problemas conexión odoo

--- production_app/static/productionApp/src/app/app.module.ts
+++ production_app/static/productionApp/src/app/app.module.ts
@@ -1,34 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { enableProdMode, ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { Network } from '@ionic-native/network';
+import { File } from '@ionic-native/file';
+
+//Paginas
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()    
   ],
   bootstrap: [IonicApp],
+
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    File,
+    Network,
   ]
 })
 export class AppModule {}
--- production_app/static/productionApp/src/pages/home/home.ts
+++ production_app/static/productionApp/src/pages/home/home.ts
@@ -2,8 +2,7 @@ import {NavController, NavParams, AlertController} from 'ionic-angular';
 import {Component} from '@angular/core';    
 import {Network} from '@ionic-native/network';
 import {Storage} from '@ionic/storage';
-import {TreepickPage} from '../../pages/treepick/treepick'; 
-import {PROXY} from '../../providers/constants/constants';
+// import {PROXY} from '../../providers/constants/constants';
 
 //import  * as odoo from '../../providers/odoo-connector/odoo.js';
 
@@ -18,10 +17,10 @@ export class HomePage {
     loginData = {password: '', username: ''};
     CONEXION = {
         url: 'http://192.168.0.119',
-        port: '069',
-        db: 'pistola',
-        username: '',
-        password: '',
+        port: '9069',
+        db: 'nogal_dev',
+        username: 'admin',
+        password: 'admin',
     };
     cargar = true;
     mensaje = '';
@@ -59,23 +58,23 @@ export class HomePage {
         alert.present();
     }
   
-  conectarApp(verificar) {
-    var self = this;
+    conectarApp(verificar) {
+        var self = this;
 
 
-    var con;
-    con = self.CONEXION;
-    this.storage.get('CONEXION').then((val) => {
-    var con;
-    if (val == null) {//no existe datos         
-        self.cargar = false;
+        var con;
         con = self.CONEXION;
-        if (con.username.length < 3 || con.password.length < 3) {
-            if (verificar) {
-                self.presentAlert('Alerta!', 'Por favor ingrese usuario y contraseña');
-            }
-        return;
-      }
+        this.storage.get('CONEXION').then((val) => {
+        var con;
+        if (val == null) {//no existe datos         
+            self.cargar = false;
+            con = self.CONEXION;
+            if (con.username.length < 3 || con.password.length < 3) {
+                if (verificar) {
+                    self.presentAlert('Alerta!', 'Por favor ingrese usuario y contraseña');
+                }
+            return;
+        }
 
         } else {
         //si los trae directamente ya fueron verificados
@@ -84,77 +83,73 @@ export class HomePage {
             return self.cargar = false;
         }
     }
-      self.cargar = true;
-      //var odoo = new Odoo(con);
 
-      var odoo = new OdooApi(PROXY, con.db);
-      odoo.login(con.username, con.password).then(
-          function (uid) {
-            
+    self.cargar = true;
+    //var odoo = new Odoo(con);
+    var odoo = new OdooApi(PROXY, con.db);
+    odoo.login(con.username, con.password).then(
+        function (uid) {
             odoo.search('res.users', [['login', '=', con.username]], ['id', 'login', 'image', 'name']).then(
-              function (value) {
+                function (value) {
             
-                var user = {id: null, name: null, image: null, login: null, cliente_id: null};
-                //self.mensaje += JSON.stringify(value);
-                if (value.length > 0) {
-                      self.storage.set('CONEXION', con);
-                      user.id = value[0].id;
-                      user.name = value[0].name;
-                      user.login = value[0].login;
-                      self.navCtrl.setRoot(TreepickPage); //-> me voy para la home page
+                    var user = {id: null, name: null, image: null, login: null, cliente_id: null};
+                    //self.mensaje += JSON.stringify(value);
+                    if (value.length > 0) {
+                        self.storage.set('CONEXION', con);
+                        user.id = value[0].id;
+                        user.name = value[0].name;
+                        user.login = value[0].login;
+                        self.navCtrl.setRoot(TreepickPage); //-> me voy para la home page
 
-                  } 
-                  else {
-                      self.cargar = false;
-                      return self.presentAlert('Falla!', 'Usuario incorrecto');
-                  } 
+                    } 
+                    else {
+                        self.cargar = false;
+                        return self.presentAlert('Falla!', 'Usuario incorrecto');
+                    } 
                 })
           })
     });
   }
 
-  getObjectId(values){
-    var self = this;
-    var object_id = {}
+    getObjectId(values){
+        var self = this;
+        var object_id = {}
 
-    var model = 'warehouse.app'
-    var method = 'get_object_id'
-    
-    this.storage.get('CONEXION').then((val) => {
-      if (val == null) {
-        console.log('No hay conexión');
-        self.navCtrl.setRoot(HomePage, {borrar: true, login: null});
-      } else {
-          console.log('Hay conexión');
-          var con = val;
-          var odoo = new OdooApi(PROXY, con.db);
-          odoo.login(con.username, con.password).then(
-            function (uid) {
-              odoo.call(model, method, values).then(
-                function (value) {
-                  object_id = value;
-                  self.cargar = false;
-                },
+        var model = 'warehouse.app'
+        var method = 'get_object_id'
+        
+        this.storage.get('CONEXION').then((val) => {
+            if (val == null) {
+                console.log('No hay conexión');
+                self.navCtrl.setRoot(HomePage, {borrar: true, login: null});
+            } else {
+                console.log('Hay conexión');
+                var con = val;
+                var odoo = new OdooApi(PROXY, con.db);
+                odoo.login(con.username, con.password).then(
+                function (uid) {
+                    odoo.call(model, method, values).then(
+                        function (value) {
+                            object_id = value;
+                            self.cargar = false;
+                        },
+                        function () {
+                            self.cargar = false;
+                            self.presentAlert('Falla!', 'Imposible conectarse');
+                        }
+                    );
+                } ,
                 function () {
-                  self.cargar = false;
-                  self.presentAlert('Falla!', 'Imposible conectarse');
-                }
-                          );
-                      },
-                      function () {
-                          self.cargar = false;
-                          self.presentAlert('Falla!', 'Imposible conectarse');
-                      }
-                  );
-                  self.cargar = false;
-
-             
-              }
-              return object_id
-              
-          });
-    
-      
-        }
+                    self.cargar = false;
+                        self.presentAlert('Falla!', 'Imposible conectarse');
+                    }
+                );
+                self.cargar = false;
+            }
+            return object_id   
+        });
+        
+          
+    }
 
 }
