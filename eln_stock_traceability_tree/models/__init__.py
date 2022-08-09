9e72f1600dc00fc7160dab25799549b9f9c35fe4
Pedro Gomez <pegomez@elnogal.com>
2022-08-09 11:50:14 +0200

    Nuevo módulo con personalizaciones del módulo stock_traceability_tree

---
+++ eln_stock_traceability_tree/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 El Nogal - Pedro Gómez <pegomez@elnogal.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import stock_traceability
+import stock_simplified_traceability
