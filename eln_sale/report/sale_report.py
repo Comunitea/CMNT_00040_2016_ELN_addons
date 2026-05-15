e7d3ae241698a67a423698e81bf3e73d3367c97e
Pedro Gómez <pegomez@elnogal.com>
2026-05-15 11:38:49 +0200

    Add product_uos_qty to SaleReport

--- eln_sale/report/sale_report.py
+++ eln_sale/report/sale_report.py
@@ -9,9 +9,10 @@ class SaleReport(models.Model):
     _inherit = "sale.report"
 
     effective_date = fields.Date(string='Effective Date', readonly=True)
+    product_uos_qty = fields.Float('Qty (UoS)', readonly=True)
 
     def _select(self):
-        return super(SaleReport, self)._select() + ", s.effective_date"
+        return super(SaleReport, self)._select() + ", s.effective_date" + ", sum(l.product_uos_qty) as product_uos_qty"
 
     def _group_by(self):
         return super(SaleReport, self)._group_by() + ", s.effective_date"
